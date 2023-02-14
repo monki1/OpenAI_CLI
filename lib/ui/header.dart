be2d34f74706ba2f056f8f2bffcf19f9c35ba95d
monki1 <kxye111@gmail.com>
2023-02-14 02:53:06 -0500

    style change

--- lib/ui/header.dart
+++ lib/ui/header.dart
@@ -6,13 +6,16 @@ import 'package:open_ai_cli/ui/component.dart';
 import 'package:open_ai_cli/ui/style.dart';
 
 Widget headerWidget(String s, Function f){
+  List<Widget> header = [Text("<<", style: headerTextStyle,),  Text(s, style: headerTextStyle,), Text("<<", style: headerTextStyle,)];
   Widget w = Container(
       // margin: EdgeInsets.all(2),
-      padding: EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
       width: double.infinity,
       alignment: Alignment.center,
       decoration: headerBoxDecoration,
-      child: Text(s, style: headerTextStyle,)
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: header,)
   );
   return gestureWidget(w, onTap: (){f();});
 }
